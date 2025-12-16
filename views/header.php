85587eaaaff4e044c715213f668895490b0ff59e
Rizky Al Farid Hafizh <alfaridrizky69@gmail.com>
2025-12-16 13:26:13 +0700

    perbaiki seo

--- views/header.php
+++ views/header.php
@@ -3,13 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplikasi Cuaca paling Gacor</title>
-    <meta name="description" content="Dapatkan ramalan cuaca akurat untuk hari ini dan prakiraan hingga 7 hari ke depan di kota mana pun di Indonesia. Update cepat dan data terpercaya.">
+    <title>Aplikasi Cuaca Terbaik Sedunia dan Galaxy</title>
+
+    <meta name="description" content="Daripada ramalan zodiak, mending ramalan cuaca 7 hari dari kita! Dijamin lebih akurat (dan lebih penting buat jemuran). Cek sekarang, sebelum kehujanan tiba-tiba!">
     <meta name="keywords" content="cuaca hari ini, ramalan cuaca, prakiraan cuaca, cuaca Bandung, cuaca 7 hari, cek cuaca, informasi cuaca">
     <link rel="canonical" href="https://cuaca-gacor.vercel.app/">  
     <meta property="og:type" content="website">
-    <meta property="og:title" content="Cuaca Paling Gacor: Prakiraan Akurat 7 Hari">  
-    <meta property="og:url" content="https://cuaca-gacor.vercel.app/"> <meta property="og:image" content="https://cuaca-gacor.vercel.app/images/cuaca-share-image.jpg"> <meta property="og:site_name" content="Aplikasi Cuaca Gacor">
+    <meta property="og:title" content="Aplikasi Cuaca Terbaik Di Dunia">  
+
+    <meta property="og:description" content="Daripada ramalan zodiak, mending ramalan cuaca 7 hari dari kita! Dijamin lebih akurat (dan lebih penting buat jemuran). Cek sekarang, sebelum kehujanan tiba-tiba!">
+    
+    <meta property="og:url" content="https://cuaca-gacor.vercel.app/"> <meta property="og:image" content="https://cuaca-gacor.vercel.app/images/cuaca-share-image.jpg">
+    <meta property="og:site_name" content="Aplikasi Cuaca Gacor">
     <meta property="og:locale" content="id_ID">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
